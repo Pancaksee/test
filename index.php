2bc801a9ff2019e5ea5a03654d38e036c4ba8da9
Chris Chrome <christophercookman@gmail.com>
2021-06-28 05:22:58 -0400

    Empty page yes

--- index.php
+++ index.php
@@ -1,31 +1,54 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SugarcaneMC</title>
-    <link rel="stylesheet" href="assets/css/style.css" />
-    <meta property="og:title" content="SugarcaneMC" />
-    <meta property="og:description" content="SugarcaneMC - The high performance Minecraft server you've always wished for!"/>
-    <meta property="og:url" content="https://sugarcanemc.org/" />
-  </head>
-  <body>
-    <center>
-      <img src="assets/img/logo.svg" id="logo" alt="logo" />
-      <h1>SugarcaneMC</h1>
-      <h2>
-        A highly optimized <a href="https://airplane.gg/">Airplane</a> fork
-        focusing on stability and performance.
-      </h2>
-      <!--<a href="#">-->
-      <h3>Downloads coming soon!</h3>
-      <!--</a>-->
-      <a href="https://github.com/SugarcaneMC/Sugarcane">
-        <img alt="GitHubLogo" src="/assets/img/github.png" width="5%" />
-      </a>
-      <a href="https://discord.gg/B6PNv2gav8">
-        <img alt="DiscordLogo" src="/assets/img/discord.png" width="5%" />
-      </a>
-    </center>
-  </body>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>SugarcaneMC</title>
+  <link rel="stylesheet" href="https://bootswatch.com/5/cyborg/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.3/css/all.css">
+  <link rel="stylesheet" href="/assets/css/style.css">
+  <meta property="og:title" content="SugarcaneMC" />
+  <meta property="og:description" content="SugarcaneMC - The high performance Minecraft server you've always wished for!" />
+  <meta property="og:url" content="https://sugarcanemc.org/" />
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/"><img src="/assets/img/logo.svg" width="40px">SugarcaneMC</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Home
+              <span class="visually-hidden">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+        <span style="font-size: 25px; margin-right: 10px;">
+          <a href="https://github.com/SugarcaneMC/"><i class="fab fa-github"></i></a>
+          <a href="/discord"><i class="fab fa-discord"></i></a>
+          <a href="https://ci.sugarcanemc.org"><i class="fas fa-server"></i></a>
+        </span>
+      </div>
+    </div>
+  </nav>
+
+  <footer class="bg-light text-center ">
+    <div class="text-center p-3">
+      &copy; 2021-<?=date("Y")?> 
+      <a class="text-dark" href="https://sugarcanemc.org/">SugarcaneMC</a>
+    </div>
+  </footer>
+
+</body>
+
 </html>
