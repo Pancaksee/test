b9f6856d51aa0ffa9ddad3d8ac612cc050adb477
Chris Chrome <christophercookman@gmail.com>
2021-07-11 13:50:39 -0400

    More styling updates, god help us all

--- team/index.php
+++ team/index.php
@@ -18,8 +18,7 @@
       text-align: center;
     } */
     a:link {
-      text-decoration: none;
-      2
+      text-decoration: none;2
     }
 
     a:visited {
@@ -46,101 +45,110 @@
       <div class="row text-center">
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <img src="/assets/img/team/nevetS.gif" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0"> Steven Tracey </h5><span class="small text-uppercase text-muted">Core Team</span>
-          <ul class="social mb-0 list-inline mt-3">
-            <li class="list-inline-item"><a href="http://nevets.tech/" class="social-link"><i style="color: #FFF;" class="far fa-browser"></i></a></li>
-            <li class="list-inline-item"><a href="https://github.com/nevetS-718" class="social-link"><i style="color: #FFF;" class="fab fa-github"></i></a></li>
-            <li class="list-inline-item"><a href="https://twitter.com/StevenTracey17" class="social-link"><i style="color: #FFF;" class="fab fa-twitter"></i></i></a></li>
-          </ul>
+          <div class="py-5 px-4"><img src="/assets/img/team/nevetS.gif" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0"> Steven Tracey </h5><span class="small text-uppercase text-muted">Core Team</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="http://nevets.tech/" class="social-link"><i style="color: #FFF;" class="far fa-browser"></i></a></li>
+              <li class="list-inline-item"><a href="https://github.com/nevetS-718" class="social-link"><i style="color: #FFF;" class="fab fa-github"></i></a></li>
+              <li class="list-inline-item"><a href="https://twitter.com/StevenTracey17" class="social-link"><i style="color: #FFF;" class="fab fa-twitter"></i></i></a></li>
+            </ul>
+          </div>
         </div>
         <!-- End-->
 
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <img src="/assets/img/team/TheArcaneBrony.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">The Arcane Brony</h5><span class="small text-uppercase text-muted">Core Team</span>
-          <ul class="social mb-0 list-inline mt-3">
-            <li class="list-inline-item"><a href="https://thearcanebrony.net/" class="social-link"><i style="color: #FFF;" class="far fa-browser"></i></a></li>
-            <li class="list-inline-item"><a href="https://github.com/TheArcaneBrony" class="social-link"><i style="color: #FFF;" class="fab fa-github"></i></a></li>
-            <li class="list-inline-item"><a href="https://gitlab.com/TheArcaneBrony" class="social-link"><i style="color: #FFF;" class="fab fa-gitlab"></i></a></li>
-            <li class="list-inline-item"><a href="https://twitter.com/TheArcaneBrony" class="social-link"><i style="color: #FFF;" class="fab fa-twitter"></i></i></a></li>
-          </ul>
+          <div class="py-5 px-4"><img src="/assets/img/team/TheArcaneBrony.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">The Arcane Brony</h5><span class="small text-uppercase text-muted">Core Team</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="https://thearcanebrony.net/" class="social-link"><i style="color: #FFF;" class="far fa-browser"></i></a></li>
+              <li class="list-inline-item"><a href="https://github.com/TheArcaneBrony" class="social-link"><i style="color: #FFF;" class="fab fa-github"></i></a></li>
+              <li class="list-inline-item"><a href="https://gitlab.com/TheArcaneBrony" class="social-link"><i style="color: #FFF;" class="fab fa-gitlab"></i></a></li>
+              <li class="list-inline-item"><a href="https://twitter.com/TheArcaneBrony" class="social-link"><i style="color: #FFF;" class="fab fa-twitter"></i></i></a></li>
+            </ul>
+          </div>
         </div>
         <!-- End-->
 
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <img src="/assets/img/team/greysilly.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">greysilly7</h5><span class="small text-uppercase text-muted">Core Team</span>
-          <ul class="social mb-0 list-inline mt-3">
-            <li class="list-inline-item"><a href="https://github.com/greysilly7" class="social-link"><i style="color: #FFF;" class="fab fa-github"></i></a></li>
-          </ul>
+          <div class="py-5 px-4"><img src="/assets/img/team/greysilly.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">greysilly7</h5><span class="small text-uppercase text-muted">Core Team</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="https://github.com/greysilly7" class="social-link"><i style="color: #FFF;" class="fab fa-github"></i></a></li>
+            </ul>
+          </div>
         </div>
         <!-- End-->
 
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <img src="/assets/img/team/tofik.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">Tofik</h5><span class="small text-uppercase text-muted">Core Team</span>
-          <ul class="social mb-0 list-inline mt-3">
-            <li class="list-inline-item"><a href="https://github.com/Toffikk" class="social-link"><i style="color: #FFF;" class="fab fa-github"></i></a></li>
-          </ul>
+          <div class="py-5 px-4"><img src="/assets/img/team/tofik.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Tofik</h5><span class="small text-uppercase text-muted">Core Team</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="https://github.com/Toffikk" class="social-link"><i style="color: #FFF;" class="fab fa-github"></i></a></li>
+            </ul>
+          </div>
         </div>
         <!-- End-->
         <br>
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <img src="/assets/img/team/ChrisChrome.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">Chris Chrome</h5><span class="small text-uppercase text-muted">Core Team Helper | Web Designer</span>
-          <ul class="social mb-0 list-inline mt-3">
-            <li class="list-inline-item"><a href="https://chrischromeyt.com/" class="social-link"><i style="color: #FFF;" class="far fa-browser"></i></a></li>
-            <li class="list-inline-item"><a href="https://twitter.com/LeChrisChrome" class="social-link"><i style="color: #FFF;" class="fa fa-twitter"></i></a></li>
-            <li class="list-inline-item"><a href="https://github.com/ChrisChrome" class="social-link"><i style="color: #FFF;" class="fab fa-github"></i></a></li>
-            <li class="list-inline-item"><a href="https://gitlab.com/ChrisChrome" class="social-link"><i style="color: #FFF;" class="fab fa-gitlab"></i></a></li>
-            <li class="list-inline-item"><a href="https://steamcommunity.com/id/ChrisChrome/" class="social-link"><i style="color: #FFF;" class="fab fa-steam"></i></a></li>
-          </ul>
+          <div class="py-5 px-4"><img src="/assets/img/team/ChrisChrome.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Chris Chrome</h5><span class="small text-uppercase text-muted">Core Team Helper | Web Designer</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="https://chrischromeyt.com/" class="social-link"><i style="color: #FFF;" class="far fa-browser"></i></a></li>
+              <li class="list-inline-item"><a href="https://twitter.com/LeChrisChrome" class="social-link"><i style="color: #FFF;" class="fa fa-twitter"></i></a></li>
+              <li class="list-inline-item"><a href="https://github.com/ChrisChrome" class="social-link"><i style="color: #FFF;" class="fab fa-github"></i></a></li>
+              <li class="list-inline-item"><a href="https://gitlab.com/ChrisChrome" class="social-link"><i style="color: #FFF;" class="fab fa-gitlab"></i></a></li>
+              <li class="list-inline-item"><a href="https://steamcommunity.com/id/ChrisChrome/" class="social-link"><i style="color: #FFF;" class="fab fa-steam"></i></a></li>
+            </ul>
+          </div>
         </div>
         <!-- End-->
 
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <img src="/assets/img/team/3305.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">3305</h5><span class="small text-uppercase text-muted">Core Team Helper</span>
-          <ul class="social mb-0 list-inline mt-3">
-            <!-- 3305, if you're reading this, DM me some stuff to put here lol -->
-          </ul>
+          <div class="py-5 px-4"><img src="/assets/img/team/3305.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">3305</h5><span class="small text-uppercase text-muted">Core Team Helper</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <!-- 3305, if you're reading this, DM me some stuff to put here lol -->
+            </ul>
+          </div>
         </div>
         <!-- End-->
 
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <img src="/assets/img/team/Ixogamer.gif" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">Ixogamer</h5><span class="small text-uppercase text-muted">Core Team Helper</span>
-          <ul class="social mb-0 list-inline mt-3">
-            <li class="list-inline-item"><a href="https://twitter.com/Ixogamer_" class="social-link"><i style="color: #FFF;" class="fa fa-twitter"></i></a></li>
-            <li class="list-inline-item"><a href="https://github.com/Ixogamer" class="social-link"><i style="color: #FFF;" class="fab fa-github"></i></a></li>
-          </ul>
+          <div class="py-5 px-4"><img src="/assets/img/team/Ixogamer.gif" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Ixogamer</h5><span class="small text-uppercase text-muted">Core Team Helper</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="https://twitter.com/Ixogamer_" class="social-link"><i style="color: #FFF;" class="fa fa-twitter"></i></a></li>
+              <li class="list-inline-item"><a href="https://github.com/Ixogamer" class="social-link"><i style="color: #FFF;" class="fab fa-github"></i></a></li>
+            </ul>
+          </div>
         </div>
         <!-- End-->
 
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <img src="/assets/img/team/kylepoops.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">Kylepoops</h5><span class="small text-uppercase text-muted">Core Team Helper</span>
-          <ul class="social mb-0 list-inline mt-3">
-            <!-- Kylepoops, if you're reading this, DM me some stuff to put here lol -->
-          </ul>
+          <div class="py-5 px-4"><img src="/assets/img/team/kylepoops.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Kylepoops</h5><span class="small text-uppercase text-muted">Core Team Helper</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <!-- Kylepoops, if you're reading this, DM me some stuff to put here lol -->
+            </ul>
+          </div>
         </div>
         <!-- End-->
 
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <img src="/assets/img/team/sholomon.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">Sholomon</h5><span class="small text-uppercase text-muted">Core Team Helper</span>
-          <ul class="social mb-0 list-inline mt-3">
-            <!-- Sholomon, if you're reading this, DM me some stuff to put here lol -->
-          </ul>
+          <div class="py-5 px-4"><img src="/assets/img/team/sholomon.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Sholomon</h5><span class="small text-uppercase text-muted">Core Team Helper</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <!-- Sholomon, if you're reading this, DM me some stuff to put here lol -->
+            </ul>
+          </div>
         </div>
         <!-- End-->
       </div>
