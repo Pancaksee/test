6f9800d71c71484f4b47e84327fff960a7824820
Chris Chrome <christophercookman@gmail.com>
2021-07-11 12:11:27 -0400

    Attempt to make navbar universal

--- index.php
+++ index.php
@@ -16,31 +16,7 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/"><img src="/assets/img/logo.svg" width="40px">SugarcaneMC</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarColor02">
-        <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Home
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/team">Team</a>
-        </li>
-        </ul>
-        <span style="font-size: 25px; margin-right: 10px;">
-          <a href="/github"><i class="fab fa-github"></i></a>
-          <a href="/discord"><i class="fab fa-discord"></i></a>
-          <a href="https://ci.sugarcanemc.org"><i class="fas fa-server"></i></a>
-        </span>
-      </div>
-    </div>
-  </nav>
+  <?=include("/assets/html/navbar.php");?>
   <div class="center-vh">
     <img src="assets/img/logo.svg" width="400" id="logo" alt="logo" /></br>
     <span id="title">SugarcaneMC</span></br>
