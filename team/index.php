a47f27bc25451f35c93b83ded948b848ad116b81
Chris Chrome <christophercookman@gmail.com>
2021-07-11 13:44:45 -0400

    Add Staff images and profile cards

--- team/index.php
+++ team/index.php
@@ -10,6 +10,13 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.3/css/all.css">
   <link rel="stylesheet" href="/assets/css/style.css">
+  <style>
+    body {
+      overflow: default !important;
+      overflow-y: default !important;
+      overflow-x: default !important;
+    }
+  </style>
   <meta property="og:title" content="SugarcaneMC" />
   <meta property="og:description" content="SugarcaneMC - The high performance Minecraft server you've always wished for!" />
   <meta property="og:url" content="https://sugarcanemc.org/" />
@@ -19,23 +26,15 @@
   <?php include($_SERVER['DOCUMENT_ROOT'] . "/assets/html/navbar.php"); ?>
   <div class="center-vh">
     <div class="container py-5">
-      <div class="row mb-4">
-        <div class="col-lg-5">
-          <h2 class="display-4 font-weight-light">Our team</h2>
-          <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        </div>
-      </div>
-
       <div class="row text-center">
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <div class="rounded shadow-sm py-5 px-4"><img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-            <h5 class="mb-0">Manuella Nevoresky</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <div class="rounded shadow-sm py-5 px-4"><img src="/assets/img/team/nevetS.gif" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0"> Steven Tracey </h5><span class="small text-uppercase text-muted">Core Team</span>
             <ul class="social mb-0 list-inline mt-3">
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="http://nevets.tech/" class="social-link"><i class="far fa-browser"></i></a></li>
+              <li class="list-inline-item"><a href="https://github.com/nevetS-718" class="social-link"><i class="fab fa-github"></i></a></li>
+              <li class="list-inline-item"><a href="https://twitter.com/StevenTracey17" class="social-link"><i class="fab fa-twitter"></i></i></a></li>
             </ul>
           </div>
         </div>
@@ -43,13 +42,13 @@
 
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <div class="rounded shadow-sm py-5 px-4"><img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-            <h5 class="mb-0">Samuel Hardy</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <div class="rounded shadow-sm py-5 px-4"><img src="/assets/img/team/TheArcaneBrony.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">The Arcane Brony</h5><span class="small text-uppercase text-muted">Core Team</span>
             <ul class="social mb-0 list-inline mt-3">
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="https://thearcanebrony.net/" class="social-link"><i class="far fa-browser"></i></a></li>
+              <li class="list-inline-item"><a href="https://github.com/TheArcaneBrony" class="social-link"><i class="fab fa-github"></i></a></li>
+              <li class="list-inline-item"><a href="https://gitlab.com/TheArcaneBrony" class="social-link"><i class="fab fa-gitlab"></i></a></li>
+              <li class="list-inline-item"><a href="https://twitter.com/TheArcaneBrony" class="social-link"><i class="fab fa-twitter"></i></i></a></li>
             </ul>
           </div>
         </div>
@@ -57,13 +56,10 @@
 
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <div class="rounded shadow-sm py-5 px-4"><img src="https://bootdey.com/img/Content/avatar/avatar8.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-            <h5 class="mb-0">Tom Sunderland</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <div class="rounded shadow-sm py-5 px-4"><img src="/assets/img/team/greysilly.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">greysilly7</h5><span class="small text-uppercase text-muted">Core Team</span>
             <ul class="social mb-0 list-inline mt-3">
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="https://github.com/greysilly7" class="social-link"><i class="fab fa-github"></i></a></li>
             </ul>
           </div>
         </div>
@@ -71,18 +67,74 @@
 
         <!-- Team item-->
         <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
-          <div class="rounded shadow-sm py-5 px-4"><img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-            <h5 class="mb-0">John Tarly</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <div class="rounded shadow-sm py-5 px-4"><img src="/assets/img/team/tofik.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Tofik</h5><span class="small text-uppercase text-muted">Core Team</span>
             <ul class="social mb-0 list-inline mt-3">
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="https://github.com/Toffikk" class="social-link"><i class="fab fa-github"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- End-->
+        <br>
+        <!-- Team item-->
+        <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
+          <div class="rounded shadow-sm py-5 px-4"><img src="/assets/img/team/ChrisChrome.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Chris Chrome</h5><span class="small text-uppercase text-muted">Core Team Helper | Web Designer</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="https://chrischromeyt.com/" class="social-link"><i class="far fa-browser"></i></a></li>
+              <li class="list-inline-item"><a href="https://twitter.com/LeChrisChrome" class="social-link"><i class="fa fa-twitter"></i></a></li>
+              <li class="list-inline-item"><a href="https://github.com/ChrisChrome" class="social-link"><i class="fab fa-github"></i></a></li>
+              <li class="list-inline-item"><a href="https://gitlab.com/ChrisChrome" class="social-link"><i class="fab fa-gitlab"></i></a></li>
+              <li class="list-inline-item"><a href="https://steamcommunity.com/id/ChrisChrome/" class="social-link"><i class="fab fa-steam"></i></a></li>
             </ul>
           </div>
         </div>
         <!-- End-->
 
+        <!-- Team item-->
+        <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
+          <div class="rounded shadow-sm py-5 px-4"><img src="/assets/img/team/3305.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">3305</h5><span class="small text-uppercase text-muted">Core Team Helper</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <!-- 3305, if you're reading this, DM me some stuff to put here lol -->
+            </ul>
+          </div>
+        </div>
+        <!-- End-->
+
+        <!-- Team item-->
+        <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
+          <div class="rounded shadow-sm py-5 px-4"><img src="/assets/img/team/Ixogamer.gif" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Ixogamer</h5><span class="small text-uppercase text-muted">Core Team Helper</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <li class="list-inline-item"><a href="https://twitter.com/Ixogamer_" class="social-link"><i class="fa fa-twitter"></i></a></li>
+              <li class="list-inline-item"><a href="https://github.com/Ixogamer" class="social-link"><i class="fab fa-github"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- End-->
+
+        <!-- Team item-->
+        <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
+          <div class="rounded shadow-sm py-5 px-4"><img src="/assets/img/team/kylepoops.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Kylepoops</h5><span class="small text-uppercase text-muted">Core Team Helper</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <!-- Kylepoops, if you're reading this, DM me some stuff to put here lol -->
+            </ul>
+          </div>
+        </div>
+        <!-- End-->
+
+        <!-- Team item-->
+        <div class="col-xl-3 card text-white bg-dark mb-3 col-sm-6 mb-5">
+          <div class="rounded shadow-sm py-5 px-4"><img src="/assets/img/team/sholomon.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Sholomon</h5><span class="small text-uppercase text-muted">Core Team Helper</span>
+            <ul class="social mb-0 list-inline mt-3">
+              <!-- Sholomon, if you're reading this, DM me some stuff to put here lol -->
+            </ul>
+          </div>
+        </div>
+        <!-- End-->
       </div>
     </div>
 
