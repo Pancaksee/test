60e8f78b42b677ca0816f3019312395a4a7f8a75
Chris Chrome <christophercookman@gmail.com>
2021-09-03 08:21:39 -0400

    Reformat most of the PHP files
    oh, and updated my card on the team page lol

--- discord/index.php
+++ discord/index.php
@@ -2,17 +2,19 @@
 $json = file_get_contents('https://canary.discord.com/api/guilds/855918593497759754/widget.json');
 $data = json_decode($json);
 str_contains($_SERVER['HTTP_USER_AGENT'], "Discordbot");
-if(!str_contains($_SERVER['HTTP_USER_AGENT'], "Discordbot")) {
-  header("Location: $data->instant_invite");
+if (!str_contains($_SERVER['HTTP_USER_AGENT'], "Discordbot")) {
+	header("Location: $data->instant_invite");
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta property="og:title" content="SugarcaneMC Discord" />
-  <meta property="og:description" content="SugarcaneMC - The high performance Minecraft server you've always wished for!"/>
-  <meta property="og:url" content="<?=$data->instant_invite?>" />
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta property="og:title" content="SugarcaneMC Discord" />
+	<meta property="og:description" content="SugarcaneMC - The high performance Minecraft server you've always wished for!" />
+	<meta property="og:url" content="<?= $data->instant_invite ?>" />
 </head>
+
 </html>
--- index.php
+++ index.php
@@ -2,31 +2,31 @@
 <html>
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>SugarcaneMC - Home</title>
-  <link rel="stylesheet" href="https://bootswatch.com/5/darkly/bootstrap.min.css">
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.3/css/all.css">
-  <link rel="stylesheet" href="/assets/css/style.css">
-  <meta property="og:title" content="SugarcaneMC" />
-  <meta property="og:description" content="SugarcaneMC - The high performance Minecraft server you've always wished for!" />
-  <meta property="og:url" content="https://sugarcanemc.org/" />
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>SugarcaneMC - Home</title>
+	<link rel="stylesheet" href="https://bootswatch.com/5/darkly/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.3/css/all.css">
+	<link rel="stylesheet" href="/assets/css/style.css">
+	<meta property="og:title" content="SugarcaneMC" />
+	<meta property="og:description" content="SugarcaneMC - The high performance Minecraft server you've always wished for!" />
+	<meta property="og:url" content="https://sugarcanemc.org/" />
 </head>
 
 <body>
-  <?php include($_SERVER['DOCUMENT_ROOT']."/assets/html/navbar.php");?>
-  <div class="center-vh">
-    <img src="assets/img/logo.svg" width="400" id="logo" alt="logo" /></br>
-    <span id="title">SugarcaneMC</span></br>
-    <span id="desc">
-      A highly optimized <a href="https://purpur.pl3x.net/">Purpur</a> fork
-      focusing on stability and performance.
-    </span></br>
-    <span id="comingsoon">Downloads coming soon!</span>
-  </div>
-  <?php include($_SERVER['DOCUMENT_ROOT']."/assets/html/footer.php");?>
+	<?php include($_SERVER['DOCUMENT_ROOT'] . "/assets/html/navbar.php"); ?>
+	<div class="center-vh">
+		<img src="assets/img/logo.svg" width="400" id="logo" alt="logo" /></br>
+		<span id="title">SugarcaneMC</span></br>
+		<span id="desc">
+			A highly optimized <a href="https://purpur.pl3x.net/">Purpur</a> fork
+			focusing on stability and performance.
+		</span></br>
+		<span id="comingsoon">Downloads coming soon!</span>
+	</div>
+	<?php include($_SERVER['DOCUMENT_ROOT'] . "/assets/html/footer.php"); ?>
 
 </body>
 
